8aae7083aa6dfb3bbb5baa82bae5755d9a849ace
Alexsandro <alexsandrojunior53@gmail.com>
2022-11-03 04:15:08 -0300

    adicionado ../ aos modulos do js

--- src/scripts/services/repositories.js
+++ src/scripts/services/repositories.js
@@ -1,4 +1,4 @@
-import {baseUrl, repositoriesQuantity} from './src/scripts/variables.js'
+import {baseUrl, repositoriesQuantity} from '../src/scripts/variables.js'
 
 async function getRepositories (userName){
     const response = await fetch(`${baseUrl}/${userName}/repos?per_page=${repositoriesQuantity}`)
