0fe209de21df8640ca855cb20960bc8a7b68c1d2
Alexsandro <alexsandrojunior53@gmail.com>
2022-11-03 04:12:07 -0300

    link dos módulos alterado

--- src/scripts/services/user.js
+++ src/scripts/services/user.js
@@ -1,4 +1,4 @@
-import {baseUrl} from '/src/scripts/variables.js'
+import {baseUrl} from './src/scripts/variables.js'
 
 async function getUser (userName){
     const response = await fetch(`${baseUrl}/${userName}`)
