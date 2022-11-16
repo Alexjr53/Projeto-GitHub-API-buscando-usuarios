09d9f22b9765e641ab87702d5674c392bfe087c1
Alexsandro <alexsandrojunior53@gmail.com>
2022-11-15 22:52:04 -0300

    adicionado dois metodos novos

--- src/scripts/objects/screen.js
+++ src/scripts/objects/screen.js
@@ -1,6 +1,6 @@
 const screen = {
     userProfile: document.querySelector('.profile-data'),
-    renderUser(user){
+    renderUser(user) {
         this.userProfile.innerHTML = `<div class="info">
                                         <img src="${user.avatarUrl}" alt="foto de perfil do usuario" /> 
                                         <div class="data">
@@ -10,48 +10,51 @@ const screen = {
                                             <p>👥 Seguidores: ${user.followers}</p>
                                         </div>
                                       </div>`
+    },
 
+    renderRepositories(user) {
         let repositoriesItens = ''
-        user.repositories.forEach(repo => repositoriesItens +=  `<li>
-                                                                    <a href="${repo.html_url}" target="_blank">
+        user.repositories.forEach(repo => repositoriesItens += `<li>
+                                                                    <a href="${repo.html_url}" target="_blank"> 
                                                                         ${repo.name}
                                                                         <ul class="repositories-stats">
                                                                             <li class="stats">🍴${repo.forks_count}</li>
                                                                             <li class="stats">⭐${repo.stargazers_count}</li>
                                                                             <li class="stats">👀${repo.watchers_count}</li>
-                                                                            <li class="stats">👨‍💻${repo.language ?? '-'}</li>
+                                                                            <li class="stats">👨‍💻${repo.language ?? 'sem linguagem'}</li>
                                                                         </ul>
                                                                     </a>
                                                                 </li>`)
-
         if (user.repositories.length > 0) {
-            this.userProfile.innerHTML +=  `<div class="repositories section">
+            this.userProfile.innerHTML += `<div class="repositories section">
                                                 <h2>Repositórios</h2>
                                                 <ul>${repositoriesItens}</ul>
-                                            </div>`
+                                           </div>`
         }
+    },
 
-        let eventsItens = ''
+    renderEvents(user) {
+         let eventsItens = ''
         user.events.forEach(events => {
             if (events.type === "PushEvent") {
-                eventsItens += `<li><span>${events.repo.name}</span> - ${events.payload.commits[0].message}</li>`    
+                eventsItens += `<li><span>${events.repo.name}</span> -${events.payload.commits[0].message}</li>`
             } else if (events.type === "CreateEvent") {
-                eventsItens += `<li><span>${events.repo.name}</span> - ${events.payload.description ?? 'Sem descrição'}</li>`
+                eventsItens += `<li><span>${events.repo.name}</span> -${events.payload.description ?? 'Sem descrição'}</li>`
             } else {
                 return
             }
-            
-            })
-        if (user.events.length > 0){
+        })
+        if (user.events.length > 0) {
             this.userProfile.innerHTML += `<div class= "events">
                                                 <h2>Eventos</h2>
                                                 <ul>${eventsItens}</ul>
-                                            </div>`
+                                           </div>`
         }
     },
-    renderNotFound(){
+
+    renderNotFound() {
         this.userProfile.innerHTML = "<h3>Usuário não encontrado</h3>"
     }
 }
 
-export {screen}
+export { screen }
